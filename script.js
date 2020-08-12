c6d3cd8cef621f124ae220684d186f7c2d570e2b
Sylwia Dobrochowska <sylwia-frania@wp.pl>
2020-08-11 20:53:27 -0700

    download items

---
+++ script.js
@@ -0,0 +1,14 @@
+let $todoInput; // miejsce, gdzie użytkownik wpisuje treść
+let $alertInfo; // info o braku zadań / konieczności dodania tekstu
+let $addBtn; // przycisk ADD - dodaje nowe elementy do listy
+let $ulList; // lista zadań, tagi <ul></ul>
+let $newTask; // nowo dodany LI - nowe zadanie
+let $allTasks; // lista wszystkich dodanych zadań - LI
+let $idNumber = 0; // ID każdego nowego zadania
+
+let $popup; //pobrany popup
+let $popupInfo; // alert w popupie, jak się doda pusty tekst
+let $editedTodo; // edytowany Todo
+let $popupInput; //tekst wpisywany w inputa w popup'ie
+let $addPopupBtn; // przycisk "zatwierdź" w popup'ie
+let $closeTodoBtn //przycisk od zamykania popup'a
